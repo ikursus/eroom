723404b17c81682b992919f94e2819a9c4f47734
jomphp <gwbiz4u@gmail.com>
2016-10-25 15:39:09 +0800

    Update Borang Status

--- resources/views/borang_status.blade.php
+++ resources/views/borang_status.blade.php
@@ -1,23 +1,29 @@
-<html>
-<body>
+@extends('layouts/app')
 
-  <h1>Semak Status Tempahan Anda</h1>
-  <hr>
+@section('content')
 
-  <p>No. Tempahan: {{ $no_tempahan }}</p>
-  {{-- Ini Comment Blade --}}
-  <!-- Ini Comment HTML -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Borang Tempahan Bilik</div>
+                <div class="panel-body">
 
   <form>
 
-    <div>
+    <div class="form-group">
       <label>Email Tempahan</label>
-      <input type="email" name="email">
+      <input type="email" name="email" class="form-control">
     </div>
 
-    <button type="submit">Semak Status</button>
+    <button type="submit" class="btn btn-primary">Semak Status</button>
 
   </form>
 
-</body>
-</html>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+@endsection
